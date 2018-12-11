1be7f76930c9671a2d135b0d81558253f857064d
Your Name <faitaa@rpi.edu>
2018-12-11 16:55:09 -0500

    added some work on magic 8 ball tutorial (not done)

---
+++ Magic 8 Ball/script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("button");
+var input = document.getElementById("input");
+var answer = document.getElementById("answer");
+var eight = document.getElementById("eight");
+
+
+button.addEventListener("click", function() {
+  alert("The button was clicked!");
+});
